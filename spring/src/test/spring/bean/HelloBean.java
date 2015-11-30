974d23107937277ce418b613733c197726edbb60
samara907 <mabin.kjy@gmail.com>
2015-11-30 13:03:45 +0900

    basic option @ModelAttribute

--- spring/src/test/spring/bean/HelloBean.java
+++ spring/src/test/spring/bean/HelloBean.java
@@ -1,6 +1,7 @@
 package test.spring.bean;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,7 @@ public class HelloBean {
 	
 	@RequestMapping("/test.do")
 	//@RequestMapping(value="/test.do",method=RequestMethod.POST)
-	public String test(DTO dto) {
+	public String test(@ModelAttribute("dto") DTO dto) {
 		// http://localhost:8000/spring/test.do?msg=javajava
 		System.out.println(dto.getMsg());
 		return "/1130/test.jsp";
