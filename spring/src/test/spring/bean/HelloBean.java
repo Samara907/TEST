8f25344452380b3a8e252fbda8dda8b5db4b1fbf
samara907 <mabin.kjy@gmail.com>
2015-11-30 12:41:59 +0900

    ALL

--- spring/src/test/spring/bean/HelloBean.java
+++ spring/src/test/spring/bean/HelloBean.java
@@ -1,12 +1,11 @@
 package test.spring.bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloBean {
 	
-	@RequestMapping(value="/test.do",method=RequestMethod.GET)
+	@RequestMapping("/test.do")
 	public String test() {
 		return "/1130/test.jsp";
 	}
