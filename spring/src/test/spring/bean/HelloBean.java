59048fa08a091e934be448ded685a377d6f399a5
samara907 <mabin.kjy@gmail.com>
2015-11-30 12:42:42 +0900

    GET AND POST ALL

--- spring/src/test/spring/bean/HelloBean.java
+++ spring/src/test/spring/bean/HelloBean.java
@@ -1,11 +1,13 @@
 package test.spring.bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloBean {
 	
 	@RequestMapping("/test.do")
+	//@RequestMapping(value="/test.do",method=RequestMethod.POST)
 	public String test() {
 		return "/1130/test.jsp";
 	}
