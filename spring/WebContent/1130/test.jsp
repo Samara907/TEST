40ec36fea2ffb51c2be26aed294e78dfff3bf517
samara907 <mabin.kjy@gmail.com>
2015-11-30 13:05:10 +0900

    spring mvc version with HttpServletRequest

--- spring/WebContent/1130/test.jsp
+++ spring/WebContent/1130/test.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h2>Hello Spring -->> ${dto.msg }</h2>
+	<h2>Hello Spring -->> ${msg }</h2>
 </body>
 </html>
--- spring/src/test/spring/bean/HelloBean.java
+++ spring/src/test/spring/bean/HelloBean.java
@@ -1,5 +1,7 @@
 package test.spring.bean;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,8 @@ public class HelloBean {
 	
 	@RequestMapping("/test.do")
 	//@RequestMapping(value="/test.do",method=RequestMethod.POST)
-	public String test(@ModelAttribute("dto") DTO dto) {
-		// http://localhost:8000/spring/test.do?msg=javajava
-		System.out.println(dto.getMsg());
+	public String test(HttpServletRequest request) {
+		request.setAttribute("msg", "javajavajava");
 		return "/1130/test.jsp";
 	}
 }
