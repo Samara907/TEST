3649628db3c5784aa7ae608b618fbe38e85ca7c9
samara907 <mabin.kjy@gmail.com>
2015-11-30 12:45:57 +0900

    web url parameter dto

---
+++ spring/src/test/spring/bean/DTO.java
@@ -0,0 +1,14 @@
+package test.spring.bean;
+
+public class DTO {
+	
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
--- spring/src/test/spring/bean/HelloBean.java
+++ spring/src/test/spring/bean/HelloBean.java
@@ -1,14 +1,16 @@
 package test.spring.bean;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloBean {
 	
 	@RequestMapping("/test.do")
 	//@RequestMapping(value="/test.do",method=RequestMethod.POST)
-	public String test() {
+	public String test(DTO dto) {
+		// http://localhost:8000/spring/test.do?msg=javajava
+		System.out.println(dto.getMsg());
 		return "/1130/test.jsp";
 	}
 }
