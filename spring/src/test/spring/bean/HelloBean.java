461edec6684e8cd8aeeb5bb5d0bb3d2b3980e0e5
samara907 <mabin.kjy@gmail.com>
2015-11-30 13:26:10 +0900

    spring-servlet.xml - <context:component-scan base-package="test.spring.bean"/>

--- spring/src/test/spring/bean/HelloBean.java
+++ spring/src/test/spring/bean/HelloBean.java
@@ -1,9 +1,9 @@
 package test.spring.bean;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +11,7 @@ public class HelloBean {
 	
 	@RequestMapping("/test.do")
 	//@RequestMapping(value="/test.do",method=RequestMethod.POST)
-	public String test(HttpServletRequest request) {
+	public String test(HttpServletRequest request, HttpSession session) {
 		request.setAttribute("msg", "javajavajava");
 		return "/1130/test.jsp";
 	}
